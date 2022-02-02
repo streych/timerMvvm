6574682932ca6057e3702045d0f01f7b03641d30
V radiulov <radiulovv.a@gmail.com>
2022-02-02 17:46:45 +1000

    zip

--- app/src/main/java/com/example/oroutines_timer_stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/oroutines_timer_stopwatch/MainActivity.kt
@@ -13,13 +13,39 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var floeOne: Flow<String>
+    lateinit var floeTwo: Flow<String>
+
     private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        getFlow()
-        startFlow()
+        setupFlows()
+        zipFlows()
+        //        getFlow()
+        //        startFlow()
+    }
+
+    private fun setupFlows() {
+        floeOne = flowOf("Юрий", "Александр", "Иван").flowOn(Dispatchers.Default)
+        floeTwo = flowOf("Гагарин", "Пушкин", "Грозный").flowOn(Dispatchers.Default)
+    }
+
+    private fun zipFlows() {
+        binding?.button?.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                floeOne.zip(floeTwo)
+                { firstString, secondString ->
+                    "$firstString $secondString"
+                }.catch { e ->
+                    Log.d(TAG, "Ошибка: $e")
+                }
+                    .collect {
+                    Log.d(TAG, it)
+                }
+            }
+        }
     }
 
     private fun startFlow() {
